4f7efdccad3c3c5c7db6c4de9709b1ae343155e9
codingbunnys <41823393+codingbunnys@users.noreply.github.com>
2018-12-26 14:33:07 -0700

    finished exampleaddon installer

--- trustedaddons/installers/exampleaddoninstaller.sh
+++ trustedaddons/installers/exampleaddoninstaller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # NOTE: This requires KLSH to be preinsatlled
 # execute chmod 755 ./exampleaddoninstaller before opening this
-wget --quiet 
+wget --quiet https://raw.githubusercontent.com/codingbunnys/klsh/master/trustedaddons/addons/exampleaddon
 echo "exampleaddon" >> ${HOME}/.klsh/addons/Addons.list
