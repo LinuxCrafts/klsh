dd48e792578ed969d16ec72d72ab01e5fd85d911
codingbunnys <41823393+codingbunnys@users.noreply.github.com>
2018-12-26 14:28:15 -0700

    exampleaddoninstaller
    execute chmod 755 ./exampleaddoninstaller before opening this

---
+++ trustedaddons/installers/exampleaddoninstaller.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# NOTE: This requires KLSH to be preinsatlled
+# execute chmod 755 ./exampleaddoninstaller before opening this
+wget --quiet 
+echo "exampleaddon" >> ${HOME}/.klsh/addons/Addons.list
