53cc314522519da6ac3bf32a9a1e77c49e117291
codingbunnys <41823393+codingbunnys@users.noreply.github.com>
2019-04-23 13:21:10 +0200

    Bug Fixes in example

--- trustedaddons/installers/exampleaddoninstaller.sh
+++ trustedaddons/installers/exampleaddoninstaller.sh
@@ -3,3 +3,5 @@
 # execute chmod 755 ./exampleaddoninstaller.sh before opening this
 wget --quiet https://raw.githubusercontent.com/codingbunnys/klsh/master/trustedaddons/addons/exampleaddon
 echo "exampleaddon" >> ${HOME}/.klsh/addons/Addons.list
+mv ./exampleaddon ${HOME}/.klsh/addons/Addons.list
+echo "INSTALLATION C O M P L E T E !"
