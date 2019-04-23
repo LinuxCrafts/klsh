84661441c5e8c1fa6fdc28eafc1fd62fc6a1f06a
codingbunnys <41823393+codingbunnys@users.noreply.github.com>
2019-04-23 13:18:53 +0200

    Small update

--- trustedaddons/installers/exampleaddoninstaller.sh
+++ trustedaddons/installers/exampleaddoninstaller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # NOTE: This requires KLSH to be preinsatlled
-# execute chmod 755 ./exampleaddoninstaller before opening this
+# execute chmod 755 ./exampleaddoninstaller.sh before opening this
 wget --quiet https://raw.githubusercontent.com/codingbunnys/klsh/master/trustedaddons/addons/exampleaddon
 echo "exampleaddon" >> ${HOME}/.klsh/addons/Addons.list
